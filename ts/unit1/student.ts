fa404ad1e984a01f0dd1587c5b52697e03d99bc2
adgai19 <gaitondeaditya@gmail.com>
2019-07-08 10:06:57 +0530

    typescript types

--- ts/unit1/student.ts
+++ ts/unit1/student.ts
@@ -1,34 +1,34 @@
-class address{
-        state:string;
-        country:string;
+class address {
+        state: string;
+        country: string;
 }
-class student{
-        fname:string;
-        lname:string;
-        rollno:number;
-        marks:Array<number>
-        saddress:address;
-        fullname(){
-                console.log(this.fname+' '+this.lname);
+class student {
+        fname: string;
+        lname: string;
+        rollno: number;
+        marks: Array<number>
+        saddress: address;
+        fullname() {
+                console.log(this.fname + ' ' + this.lname);
         }
-        percent(){
-                let i:number,temp=0;
-                for(i=0;i<this.marks.length;i++){
-                        temp+=this.marks[i];
+        percent() {
+                let i: number, temp = 0;
+                for (i = 0; i < this.marks.length; i++) {
+                        temp += this.marks[i];
                 }
-                temp/=this.marks.length;
+                temp /= this.marks.length;
                 return temp;
         }
 }
-let addresstemp=new address();
-addresstemp.state="Goa";
-addresstemp.country="India";
+let addresstemp = new address();
+addresstemp.state = "Goa";
+addresstemp.country = "India";
 let student1 = new student();
-student1.fname="john";
-student1.lname="doe";
+student1.fname = "john";
+student1.lname = "doe";
 student1.fullname();
-student1.marks=[12,23,36,98,74,45];
-student1.rollno=112358;
-student1.saddress=addresstemp;
+student1.marks = [12, 23, 36, 98, 74, 45];
+student1.rollno = 112358;
+student1.saddress = addresstemp;
 console.log(student1);
-console.log("percentage="+student1.percent());
+console.log("percentage=" + student1.percent());
--- ts/unit1/tsDatatypes.ts
+++ ts/unit1/tsDatatypes.ts
@@ -18,8 +18,47 @@ console.log(f3(3,'b'))
  }
  console.log(a);
  */
- var a=10;
+/*  var a=10;
  if(true){
         let  a=90;
  }
  console.log(a)
+ */
+/* 
+ var a//type anything
+ a=10;
+ a="hello";
+ a=true;
+ console.log(a);
+ */
+/* 
+ var a:number//type number
+ a=10;
+ a="hello";
+ a=true;
+ console.log(a); */
+/* 
+ var a:string;//type number
+ a=10;
+ a="hello";
+ a=true;
+ console.log(a);
+ *//* 
+var a:boolean;//type number
+a=10;
+a="hello";
+a=true;
+console.log(a); */
+/* 
+ var arr:number[];
+ arr=[9,8,4,5];
+ arr.push(3);
+ console.log(arr);
+ let i;
+ for(i=0;i<arr.length;i++){
+  
+ console.log(arr.pop());       
+ } */
+var tuple: [number, boolean, string];
+tuple = [23, true, "hello world"];
+console.log(tuple);
