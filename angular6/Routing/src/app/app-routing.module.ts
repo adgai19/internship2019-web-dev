e1c8461d2fc2e6c655a1c132967db973fcccfed2
adgai19 <gaitondeaditya@gmail.com>
2019-07-12 11:50:03 +0530

    routing + new project

--- angular6/Routing/src/app/app-routing.module.ts
+++ angular6/Routing/src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CompComponent } from './comp/comp.component';
+import { ProductsComponent } from './products/products.component';
 
 
-const routes: Routes = [];
+const routes: Route[] = [
+  { path: '', redirectTo: '/home', pathMatch: "full" },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'aboutus', component: AboutUsComponent, children: [
+      { path: '', redirectTo: 'comp', pathMatch: 'full' }
+      , {
+        path: 'comp', component: CompComponent
+      },
+      { path: 'products', component: ProductsComponent }
+    ]
+
+
+  },
+  { path: '**', component: PageNotFoundComponent }//last object page not found
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
