30e5a2bcf6d3fbecd59923bef466eac0cd1248d5
adgai19 <gaitondeaditya@gmail.com>
2019-07-13 21:52:24 +0530

    assignment

--- angular6/assignment/src/app/app-routing.module.ts
+++ angular6/assignment/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { EnquieryComponent } from './enquiery/enquiery.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Route[] = [
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'product',component:ProductsComponent},
+  {path:'enquiry',component:EnquieryComponent},
+  {path:'gallary',component:GallaryComponent},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular6/assignment/src/app/app.module.ts
+++ angular6/assignment/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { EnquieryComponent } from './enquiery/enquiery.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    EnquieryComponent,
+    GallaryComponent,
+    AboutusComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
